50f37a753a08eea27aff72458958ffa1cd1e77f6
Sohee52 <wneh2023@naver.com>
2025-10-03 22:11:21 +0900

    1916

--- 3-Dijkstra/1916.cpp
+++ 3-Dijkstra/1916.cpp
@@ -13,8 +13,8 @@ using pll = pair<ll, ll>;
 priority_queue <pll, vector<pll>, greater<pll>> pq;
 
 int main(){
-    ios::sync_with_stdio(0); // fastio
-    cin.tie(0), cout.tie(0); // fastio
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
 
     cin >> n >> m;
     while(m--){
@@ -23,23 +23,16 @@ int main(){
     }
     ll s, e; cin >> s >> e;
 
-    // 거리 배열 큰 값으로 초기화
-    for(int i = 0;i < MAX;i++) d[i] = INF;
-    // 시작 정점을 방문 가능한 정점으로 처리
+    for(int i = 0;i < MAX; i++) d[i] = INF;
     pq.push({0, s});
 
     while(!pq.empty()){
-        // 방문 가능한 정점 중 최단 거리인 정점 방문
         auto[cd, cur] = pq.top(); pq.pop();
-        // 이미 최단거리이면 건너 뜀
         if(d[cur] <= cd) continue;
-        // 거리 갱신
         d[cur] = cd;
 
         for(auto& [nxt, co] : adj[cur]){
-            // 다음 정점이 이미 최단거리이면 건너 뜀
             if(d[nxt] <= cd + co) continue;
-            // 방문 가능한 정점에 포함
             pq.push({cd + co, nxt});
         }
     }
