0560f36298aaefac056ac9f63ed39944d9668f82
Sohee52 <wneh2023@naver.com>
2025-10-04 02:18:35 +0900

    1916. 최소 비용 구하기기

--- 3-Dijkstra/1916-m.cpp
+++ 3-Dijkstra/1916-m.cpp
@@ -1,47 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define end aaa
+typedef long long ll;
+typedef pair<ll, ll> pll;
 const int INF = 1e9;
-int n, m, s, e, c, st, ed, dist[1004];
-vector<pair<int, int>> adj[1004];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+vector<pll> adj[1004];
+int n, m, s, e, c, start, end, dist[1004];
+priority_queue<pll, vector<pll>, greater<pll>> pq;
 
 void dijkstra(int start) {
-    fill(dist, dist + 1004, INF);
+	fill(dist, dist+1004, INF);
+	dist[start] = 0;
+	pq.push({0, start});
 
-    dist[start] = 0;
-    pq.push({0, start});
-
-    while (pq.size()) {
-        int here = pq.top().second;
-        int here_dist = pq.top().first;
-        pq.pop();
-
-        if (dist[here] != here_dist) continue;
-
-        for (auto there : adj[here]) {
-            int _dist = there.first;
-            int _there = there.second;
-
-            if (dist[_there] > dist[here] + _dist) {
-                dist[_there] = dist[here] + _dist;
-                pq.push({dist[_there], _there});
-            }
-        }
-    }
+	while(pq.size()) {
+		auto [hd, here] = pq.top(); pq.pop();
+		if (dist[here] != hd) continue;
+		for (auto &[there, td] : adj[here]) {
+			int nd = hd + td;
+			if (dist[there] > nd) {
+				dist[there] = nd;
+				pq.push({nd, there});
+			}
+		}
+	}
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> s >> e >> c;
-        adj[s].push_back({c, e});
-    }
-    cin >> st >> ed;
+	cin >> n >> m;
+	for (int i = 0;i < m; i++) {
+		cin >> s >> e >> c;
+		adj[s].push_back({e, c});
+	}
+	cin >> start >> end;
 
-    dijkstra(st);
-    cout << dist[ed] << "\n";
+	dijkstra(start);
+
+	cout << dist[end] << "\n";
 }
-
