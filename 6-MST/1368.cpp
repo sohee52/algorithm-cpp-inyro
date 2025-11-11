4c0aba4f52ae222ce7a4e0e96d588d6245799368
sohee52 <wneh2023@naver.com>
2025-11-11 23:34:11 +0900

    1368. 물대기 (ing)

---
+++ 6-MST/1368.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+    for (int i = 0; i < n; i++) {
+
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            
+        }
+    }
+}
